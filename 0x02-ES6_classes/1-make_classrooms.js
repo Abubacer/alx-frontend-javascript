f30f79408521ef3dc6ea9e875fe6bffe60a0b145
Abubacer <azizdesignislife@gmail.com>
2024-01-06 02:57:56 +0000

    ES6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
 import ClassRoom from './0-classroom';
 
 /**
- * Creates an array of {@link ClassRoom}s with a specific size.
+ * Imports the ClassRoom class.
+ * Creates an array of {@link ClassRoom}s with a specific size using map.
  * @returns An array of {@link ClassRoom}s.
  */
 export default function initializeRooms() {
