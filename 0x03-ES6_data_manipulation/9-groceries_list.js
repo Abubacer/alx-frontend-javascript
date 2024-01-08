4986f434ba9ca04bbe18cbdc72332cd72817b2c0
Abubacer <azizdesignislife@gmail.com>
2024-01-08 21:52:40 +0000

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+// returns a map of groceries with the following items (name, quantity).
+function groceriesList() {
+  const mapGroceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  return mapGroceries;
+}
+
+export default groceriesList;
