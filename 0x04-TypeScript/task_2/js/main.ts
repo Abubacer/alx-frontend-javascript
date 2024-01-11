7047a77792e7878305c253f33732a30f3392c92f
Abubacer <azizdesignislife@gmail.com>
2024-01-11 16:01:52 +0000

    Create the DirectorInterface, create isDirector, executeWork and teachClass function

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,93 @@
+// Define an interface named DirectorInterface.
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// Define an interface named TeacherInterface.
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Define a Director class
+export class Director implements DirectorInterface {
+    workFromHome(): string {
+        console.log("Working from home");
+        return "Working from home";
+    }
+    getCoffeeBreak(): string {
+        console.log("Getting a coffee break");
+        return "Getting a coffee break";
+    }
+    workDirectorTasks(): string {
+        console.log("Getting to director tasks");
+        return "Getting to director tasks";
+    }
+}
+
+// Define a Teacher class
+export class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        console.log("Cannot work from home");
+        return "Cannot work from home";
+    }
+    getCoffeeBreak(): string {
+        console.log("Cannot have a break");
+        return "Cannot have a break";
+    }
+    workTeacherTasks(): string {
+        console.log("Getting to work");
+        return "Getting to work";
+    }
+}
+
+// Return either a Director or a Teacher instance based on salary number
+export function createEmployee(salary: number | string): Teacher | Director {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+// console.log(createEmployee(200));
+// console.log(createEmployee(1000));
+// console.log(createEmployee('$500'));
+
+// Execute the appropriate work task based on the type of the employee,
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+    return 'workDirectorTasks' in employee;
+}
+
+export function executeWork(employee: TeacherInterface | DirectorInterface): string {
+    let taskCall: string;
+    if (isDirector(employee)) {
+        taskCall = employee.workDirectorTasks();
+    } else {
+        taskCall = employee.workTeacherTasks();
+    }
+    console.log(taskCall);
+    return taskCall;
+}
+
+// executeWork(createEmployee(200));
+// executeWork(createEmployee(1000));
+
+// Define a type for subjects that can be taught
+type Subjects = "Math" | "History";
+// Return today class based on the specified subject
+export function teachClass(todayClass: Subjects): string {
+    switch (todayClass) {
+        case "History":
+            return "Teaching History";
+        case "Math":
+            return "Teaching Math";
+        default:
+            return "Enjoy Your Free Time";
+    }
+}
+
+// console.log(teachClass('Math'));
+// console.log(teachClass('History'));
